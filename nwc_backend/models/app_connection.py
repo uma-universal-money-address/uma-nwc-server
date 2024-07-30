28672cd9a3b986f44abe775fd505b51995642483
Dawit Aboye <dawit@lightspark.com>
2024-07-30 10:33:14 -0700

    Only have supported commands

--- nwc_backend/models/app_connection.py
+++ nwc_backend/models/app_connection.py
@@ -18,26 +18,17 @@ class AppConnection(ModelBase):
     app_name = Column(String(255))
     description = Column(String(255))
     nostr_pubkey = Column(String(255), unique=True)
-    required_commands = Column(Text)  # Store JSON as string
-    optional_commands = Column(Text)  # Store JSON as string
+    supported_commands = Column(Text)  # Store JSON as string
     max_budget_per_month = Column(Integer)
     expires_at = Column(TIMESTAMP(timezone=True))
     long_lived_vasp_token = Column(String(255))
 
-    def set_required_commands(self, commands: list[Nip47RequestMethod]) -> None:
+    def set_supported_commands(self, commands: list[Nip47RequestMethod]) -> None:
         commands_vals = [command.value for command in commands]
-        self.required_commands = json.dumps(commands_vals)
+        self.supported_commands = json.dumps(commands_vals)
 
-    def get_required_commands(self) -> list[Nip47RequestMethod]:
-        command_vals = json.loads(self.required_commands)
-        return [Nip47RequestMethod(command) for command in command_vals]
-
-    def set_optional_commands(self, commands: list[Nip47RequestMethod]) -> None:
-        commands_vals = [command.value for command in commands]
-        self.optional_commands = json.dumps(commands_vals)
-
-    def get_optional_commands(self) -> list[Nip47RequestMethod]:
-        command_vals = json.loads(self.optional_commands)
+    def get_supported_commands(self) -> list[Nip47RequestMethod]:
+        command_vals = json.loads(self.supported_commands)
         return [Nip47RequestMethod(command) for command in command_vals]
 
     @staticmethod
