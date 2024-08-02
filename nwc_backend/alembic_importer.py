32ce0347fe6afcf28ba9f7d29c7363e79f8b8a04
Yun Yu <yun@lightspark.com>
2024-08-02 14:37:06 -0700

    Create Nip47Request model

--- nwc_backend/alembic_importer.py
+++ nwc_backend/alembic_importer.py
@@ -2,6 +2,7 @@
 # pyre-strict
 # ruff: noqa: F401
 
-from nwc_backend.models.user import User
 from nwc_backend.models.app_connection import AppConnection
+from nwc_backend.models.nip47_request import Nip47Request
 from nwc_backend.models.nwc_connection import NWCConnection
+from nwc_backend.models.user import User
