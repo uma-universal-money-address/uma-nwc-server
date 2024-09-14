a1862a157acaaaacd68f3bb146d45dc08af06879
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2024-09-13 17:09:31 -0700

    [frontend] add permission type for receive payments

--- nwc-frontend/src/utils/backendDomain.ts
+++ nwc-frontend/src/utils/backendDomain.ts
@@ -1,6 +1,3 @@
-import assert from "assert";
-import isDevelopment from "./isDevelopment";
-
 export const getBackendDomain = () => {
   if (typeof window !== "undefined") {
     return window.location.host;
