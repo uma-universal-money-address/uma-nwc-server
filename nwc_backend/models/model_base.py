4aa995fbc4b0362325d924ba672c69f643b6c458
Jeremy Klein <jklein24@gmail.com>
2024-09-19 00:03:11 -0700

    Test works

--- nwc_backend/models/model_base.py
+++ nwc_backend/models/model_base.py
@@ -6,12 +6,13 @@ from uuid import UUID
 
 import sqlalchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
 from nwc_backend.db import UUID as DBUUID
 from nwc_backend.db import DateTime
 
 
-class ModelBase(DeclarativeBase):
+class ModelBase(DeclarativeBase, AsyncAttrs):
     def __setitem__(self, key: str, value: Any) -> None:
         return setattr(self, key, value)
 
