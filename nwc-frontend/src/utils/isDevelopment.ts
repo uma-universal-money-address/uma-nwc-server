a48d451e64223dbfe4d28253d43211f5ceff86c3
Jeremy Klein <jklein24@gmail.com>
2024-09-24 09:22:45 -0700

    Remove unused ts-expect-error

--- nwc-frontend/src/utils/isDevelopment.ts
+++ nwc-frontend/src/utils/isDevelopment.ts
@@ -1,3 +1,2 @@
-// @ts-expect-error - process is showing up as undefined.
-const isDevelopment = process?.env?.NODE_ENV === "development";
+const isDevelopment = process.env.NODE_ENV === "development";
 export default isDevelopment;
