9de4c2b9f309788b9441778e7b1bcdc967a12ef5
Jeremy Klein <jklein24@gmail.com>
2024-10-01 11:41:19 -0700

    Merge fix for tests

--- nwc_backend/api_handlers/__tests__/update_connection_test.py
+++ nwc_backend/api_handlers/__tests__/update_connection_test.py
@@ -4,12 +4,12 @@ from datetime import datetime, timezone
 
 import pytest
 from quart.app import QuartClient
+
 from nwc_backend.db import db
-
-
 from nwc_backend.models.__tests__.model_examples import (
     create_nwc_connection,
     create_spending_limit,
+    jwt_for_user,
 )
 from nwc_backend.models.nwc_connection import NWCConnection
 from nwc_backend.models.spending_limit import SpendingLimit
@@ -21,8 +21,8 @@ async def test_update_connection_success(
 ) -> None:
     async with test_client.app.app_context():
         connection: NWCConnection = await create_nwc_connection()
-    async with test_client.session_transaction() as session:
-        session["user_id"] = connection.user_id
+        token = jwt_for_user(connection.user)
+
     amount_in_lowest_denom = 7000
     limit_frequency = "monthly"
     expiration = "2029-09-25T01:22:08.388Z"
@@ -36,7 +36,9 @@ async def test_update_connection_success(
     }
 
     response = await test_client.post(
-        f"/api/connection/{connection.id}", json=request_data
+        f"/api/connection/{connection.id}",
+        json=request_data,
+        headers={"Authorization": f"Bearer {token}"},
     )
     async with test_client.app.app_context():
         updated_connection = await db.session.get(NWCConnection, connection.id)
@@ -55,10 +57,9 @@ async def test_update_connection_disable_limit_success(
 ) -> None:
     async with test_client.app.app_context():
         connection: NWCConnection = await create_nwc_connection()
-        spendnd_limit = await create_spending_limit(nwc_connection=connection)
+        spend_limit = await create_spending_limit(nwc_connection=connection)
+        token = jwt_for_user(connection.user)
 
-    async with test_client.session_transaction() as session:
-        session["user_id"] = connection.user_id
     request_data = {
         "limitEnabled": False,
         "expiration": "2029-09-25T01:22:08.388Z",
@@ -66,11 +67,13 @@ async def test_update_connection_disable_limit_success(
     }
 
     response = await test_client.post(
-        f"/api/connection/{connection.id}", json=request_data
+        f"/api/connection/{connection.id}",
+        json=request_data,
+        headers={"Authorization": f"Bearer {token}"},
     )
     async with test_client.app.app_context():
         updated_connection = await db.session.get(NWCConnection, connection.id)
-        updated_limit = await db.session.get(SpendingLimit, spendnd_limit.id)
+        updated_limit = await db.session.get(SpendingLimit, spend_limit.id)
 
     assert response.status_code == 200
     assert updated_connection.spending_limit is None
@@ -83,14 +86,16 @@ async def test_delete_connection_success(
 ) -> None:
     async with test_client.app.app_context():
         connection: NWCConnection = await create_nwc_connection()
-    async with test_client.session_transaction() as session:
-        session["user_id"] = connection.user_id
+        token = jwt_for_user(connection.user)
+
     request_data = {
         "status": "Inactive",
     }
 
     response = await test_client.post(
-        f"/api/connection/{connection.id}", json=request_data
+        f"/api/connection/{connection.id}",
+        json=request_data,
+        headers={"Authorization": f"Bearer {token}"},
     )
     async with test_client.app.app_context():
         updated_connection = await db.session.get(NWCConnection, connection.id)
